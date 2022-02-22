22791592d0b48b71c9c218c9330a2f116a78558a
zork <adampapirnik@gmail.com>
2022-02-22 15:27:30 -0600

    Basic implementation

--- LinkedList.rb
+++ LinkedList.rb
@@ -9,5 +9,69 @@ end
 
 # Represents the entire LinkedList
 class LinkedList
-  # TODO
+  attr_accessor :head_node, :tail_node
+  
+  def initialize(node)
+    @head_node = node
+    @tail_node = node
+  end
+
+  def append(value)
+    @tail_node.next_node = Node.new(value)
+    @tail_node = @tail_node.next_node
+  end
+
+  def prepend(value)
+    node = Node.new(value)
+    node.next_node = @head_node
+    @head_node = node
+  end
+
+  def at(index)
+    curr_node = @head_node
+    for i in 0..index
+      return curr_node.value if i == index
+      return nil if curr_node.next.nil?
+    end
+  end
+
+  def find_new_tail(node)
+    return @tail_node = node if node.next_node == nil
+    find_new_tail(node.next_node)
+  end
+
+  def pop
+    @tail_node = nil
+    find_new_tail(@head_node)
+  end
+
+  def contains?(value)
+    curr_node = @head_node
+    index = 0
+    until curr_node.nil?
+      return index if curr_node.value == value
+
+      index += 1
+    end
+    nil
+  end
+
+  def to_s
+    curr_node = @head_node
+    string = ''
+    until curr_node.nil?
+      string += "( #{curr_node.value} ) -> "
+      curr_node = curr_node.next_node
+    end
+    string + 'nil'
+  end
 end
+
+linked = LinkedList.new(Node.new(1))
+
+linked.append(2)
+linked.append(3)
+linked.append(4)
+linked.append(5)
+
+puts linked.to_s
